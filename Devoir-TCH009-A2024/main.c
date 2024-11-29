9db026e7ba13aa1aacfcbbbae6e7b0d4d5a48572
LudovicConfaisCourcy <123512499+LudovicConfaisCourcy@users.noreply.github.com>
2024-11-29 17:34:50 -0500

    Add files via upload

---
+++ Devoir-TCH009-A2024/main.c
@@ -0,0 +1,201 @@
+/*  Programme d'analyse (simple) de trace GPS.
+	Auteur: David Marche
+	Date : Septembre 2013
+
+	Ce programme permet de lire un fichier de trace GPS et de realiser les calculs d’analyse suivants sur les données
+	récupérées:
+		- Distance totale parcourue
+		- Ascension totale realisee
+		- Altitude maximum/minimum atteinte
+		- Pente maximale du parcours
+
+	Supposition de simplification du probleme:
+		- La trace se passe dans une seule journee. Calcul de temps seulement sur Heure,minute,seconde.
+		- Le fichier de points doit se trouver dans le repertoire du projet
+
+*/
+
+#define _CRT_SECURE_NO_WARNINGS
+
+// Librairies standard
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <math.h>
+
+// Librairies du projet
+#include "config.h"
+#include "test.h"
+#include "menu.h"
+#include "matrac_lib.h"
+#include "menu.h"
+
+
+
+/* ---------------------------------------------------------------------------------------------------------------------
+	Fonction: charger
+	Auteur:
+	Description:
+		Cette fonction saisi un nom de fichier de donnees GPS, lit et place toutes les donnees dans des tableaux
+		statiques fournis en parametres. Le format du fichier est le suivant :
+		No,Latitude,Longitude,Altitude,Date,Time
+		1,49.192273,-123.178443,-44.6,2013/06/21,11:02:00
+		2,49.192272,-123.178318,-44.6,2013/06/21,11:02:03
+		3,49.192184,-123.178061,-41.7,2013/06/21,11:02:10
+		...
+
+	Parametres:
+		tab_latitudes	    tableau a remplir avec les valeurs de latitude
+		tab_longitudes		tableau a remplir avec les valeurs de longitude
+		tab_altitudes		tableau a remplir avec les valeurs d'altitude
+		taille_max		    nombre maximum de valeurs pouvant etre chargees dans les tableaux
+
+	Valeur de retour:
+		Le nombre de points lus, 0 si aucun, -1 si probleme lors de la lecture du fichier,
+	Note:
+		- Les tableaux donnees en parametre sont modifies.
+	Cas particuliers:
+		- S'il y a eu un problème lors de la lecture, la fonction renvoie -1 sinon elle retourne le nombre de points lus;
+*/
+
+int charger(double tab_latitudes[], double tab_longitudes[], double tab_altitudes[], int taille_max){
+
+	char nom_fichier[LONGUEUR_MAX_NOM_FICHIER] = {};
+
+	printf("Nom du fichier: ");
+	scanf("%s",nom_fichier);
+	printf("Chargement du fichier %s...",nom_fichier);
+
+	int taille1 = lire_donnees_gps(nom_fichier,tab_latitudes,tab_longitudes,tab_altitudes,taille_max);
+
+	return taille1;
+	}
+
+/* ---------------------------------------------------------------------------------------------------------------------
+	Fonction: afficher
+	Auteur:
+	Description:
+		Cette fonction affiche toutes les donnees de la trace presente en memoire avec la premiere ligne d'en-tete
+		suivante:
+			"index      lattitude     longitude   altitude"
+	Parametres:
+		tab_latitudes		tableau des valeurs de latitude
+		tab_longitudes		tableau des valeurs de longitude
+		tab_altitudes		tableau des valeurs d'altitude
+		taille			    nombre de valeurs valides presentes dans les tableaux
+	Valeur de retour:
+		Aucune.
+	Cas particuliers:
+		- Quand la taille est nulle ou negative, seule la ligne d'en-tete est affichee.
+*/
+
+void afficher(double tab_latitudes[], double tab_longitudes[], double tab_altitudes[], int taille) {
+	printf("Donnees en Memoire:\n");
+	printf("Index\tLatitude   Longitude %9-s\n","Altitude");
+	if(taille > 0) {
+		for (int i = 0; i < taille; i++) {
+			printf("%7-d %-9lf %-9lf %9-lf\n", i, tab_latitudes[i], tab_longitudes[i], tab_altitudes[i]);
+		}
+	}
+}
+
+/* ---------------------------------------------------------------------------------------------------------------------
+	Fonction: analyser
+	Auteur:
+	Description:
+		Cette fonction affiche l'analyse de la trace. Elle affiche la distance totale parcourue, l'ascension effectuee,
+		l'altidude maximale et minimale.
+	Parametres:
+		tab_latitudes		tableau des valeurs de latitude
+		tab_longitudes		tableau des valeurs de longitude
+		tab_altitudes		tableau des valeurs d'altitude
+		taille			    nombre de valeurs presentes dans les tableaux
+	Valeur de retour:
+		Aucune.
+	Cas particulier:
+		Si la valeur de la taille est nulle ou negative, les valeurs affichees seront vides.
+
+*/
+
+void analyser(double tab_latitudes[], double tab_longitudes[], double tab_altitudes[], int taille) {
+
+	double minimum = 0;
+	double maximum = 0;
+
+	printf("Analyse de la trace...\n\n");
+
+	minimum_tableau(tab_altitudes, taille, &minimum);
+	maximum_tableau(tab_altitudes, taille, &maximum);
+
+	printf("Distance parcourue:\t %8-.3lf km\n", distance_totale(tab_latitudes, tab_longitudes, tab_altitudes, taille, 0,taille));
+	printf("%20-s %8-.3lf m\n"," Ascencion:\t", ascension(tab_altitudes,taille));
+	printf("%19-s %8-.3f m\n"," Altitude maximale:\t", maximum);
+	printf("%19-s %8-.3f m\n"," Altitude minimale:\t", minimum);
+
+}
+
+/* ---------------------------------------------------------------------------------------------------------------------
+	Fonction: calculer
+	Auteur:
+	Description:
+		Cette fonction lit la taille de la fenetre pour calculer la pente maximale de la trace et affiche le resultat.
+
+	Parametres:
+		tab_latitudes		tableau des valeurs de latitude
+		tab_longitudes		tableau des valeurs de longitude
+		tab_altitudes		tableau des valeurs d'altitude
+		taille			    nombre de valeurs presentes dans les tableaux
+	Valeur de retour:
+		aucune
+*/
+
+void calculer(double tab_latitudes[], double tab_longitudes[], double tab_altitudes[], int taille) {
+
+}
+
+// Programme principal
+int main()
+{
+	double tab_latitudes[NB_MAX_POINT] = {};
+	double tab_longitudes[NB_MAX_POINT] = {};
+	double tab_altitudes[NB_MAX_POINT] = {};
+	int choix = 0;
+	int taille1 = 0;
+
+	while(choix != 5){
+	choix = afficher_menu();
+
+	switch(choix) {
+		case 1: {
+			taille1 = charger(tab_latitudes, tab_longitudes, tab_altitudes, NB_MAX_POINT);
+			break;
+		}
+
+		case 2: {
+			afficher(tab_latitudes, tab_longitudes, tab_altitudes, taille1);
+			break;
+		}
+
+		case 3: {
+			analyser(tab_latitudes, tab_longitudes, tab_altitudes, taille1);
+			break;
+		}
+
+		case 4: {
+			calculer(tab_latitudes, tab_longitudes, tab_altitudes, LONGUEUR_MAX_LIGNE);
+			printf("option 4");
+			break;
+		}
+
+		case 5: {
+			printf("Fermeture du Programme");
+			break;
+		}
+
+		default: {
+			printf("Choix Invalid");
+		}
+	}
+        }
+	return EXIT_SUCCESS;
+}
---
+++ Devoir-TCH009-A2024/matrac_lib.c
@@ -0,0 +1,174 @@
+/*
+Auteur(s):
+Date:
+
+Ce module contient les implementations des fonctions d'analyse de traces GPS
+
+*/
+
+#define _CRT_SECURE_NO_WARNINGS
+
+// Librairies standard
+#include <stdio.h>
+#include <math.h>
+
+#include "config.h"
+#include "matrac_lib.h"
+
+int lire_donnees_gps(char nom_fichier[],
+	                 double tab_latitudes[],
+	                 double tab_longitudes[],
+	                 double tab_altitudes[],
+	                 int taille_max) {
+	int nb_point = 0;						            // nombre de points trouves
+	char ligne[LONGUEUR_MAX_LIGNE];			            // chaine de caractere contenant la balise
+	FILE* fp;								            // pointeur au fichier a lire
+	int no, an, mois, jour, heure, minute, seconde;		// informations lues du fichier
+	double latitude, longitude, altitude;
+
+	// ouverture du fichier et verification du succes de l'ouverture
+	fp = fopen(nom_fichier, "r"); // en mode lecture seulement
+
+	if (fp == NULL) {
+		nb_point = -1;
+	} else {
+		// sauter l'en-tete
+		if (fgets(ligne, LONGUEUR_MAX_LIGNE - 1, fp) == NULL) {
+			nb_point = -1;
+
+			// continuer la lecture si fichier n'est pas vide
+		} else {
+
+			// boucle de lecture principale de chaque point (un par ligne)
+			while ((fgets(ligne, LONGUEUR_MAX_LIGNE - 1, fp) != NULL) && (nb_point < taille_max)) {
+				// lecture d'un point et identification des 10 champs attendus:
+				// No,Latitude,Longitude,Altitude,Date(a/m/j),Time(h:m:s)
+				if (sscanf(ligne, "%d,%lf,%lf,%lf,%d/%d/%d,%d:%d:%d", &no,
+				                                                      &latitude,
+				                                                      &longitude,
+				                                                      &altitude,
+				                                                      &an,
+				                                                      &mois,
+				                                                      &jour,
+				                                                      &heure,
+				                                                      &minute,
+				                                                      &seconde) == 10){
+
+					//si decodage correct,mise en memoire de la latitude, longitude,altitude
+					tab_latitudes[nb_point] = latitude;
+					tab_longitudes[nb_point] = longitude;
+					tab_altitudes[nb_point] = altitude;
+
+					// incrementer le nombre de points correctement lus
+					nb_point++;
+				}
+			}
+		}
+		// fermeture du fichier
+		fclose(fp);
+	}
+
+	if (nb_point >= 0) {
+		printf("\n...%d points lus\n", nb_point);
+	}else {
+		printf("\n...probleme de lecture du fichier\n");
+	}
+	//renvoi du nombre de points trouves
+	return nb_point;
+
+}
+
+// implementations des fonctions de matrac_lib (c.f. prototype et doc dans matrac_lib.h)
+
+double convertir_en_radian(double angle){
+	return angle * PI / 180;
+}
+
+double distance_entre_2_points(double latitude1,
+							   double longitude1,
+							   double latitude2,
+							   double longitude2,
+							   double altidute1,
+							   double altidute2){
+
+	double delta_lat= (convertir_en_radian(latitude2)-convertir_en_radian(latitude1))/2;
+	double lat1 = convertir_en_radian(latitude1);
+	double lat2 = convertir_en_radian(latitude2);
+	double delta_long = convertir_en_radian((longitude2-longitude1))/2;
+
+	double d = RAYON_TERRE * 2 * asin(sqrt(pow(sin(delta_lat),2) + (cos(lat1)) * cos(lat2) * pow(sin(delta_long), 2)));
+	double d_rot = sqrt(pow(d,2) + pow((altidute2 - altidute1)/1000,2));
+
+	//double d_tot = sqrt(pow(d,2) + pow(d_rot,2))/1000;
+
+	return d_rot;
+}
+
+void afficher_tableau(double tableau[], int nb_valeurs) {
+	for (int i = 0; i < (nb_valeurs); i++) {
+		printf("%lf\n", tableau[i]);
+	}
+}
+
+double distance_totale(double tab_latitudes[],
+					   double tab_longitudes[],
+					   double tab_altitudes[],
+					   int taille,
+					   int debut,
+					   int fin){
+
+	double total = 0;
+
+	if(debut < 0 || fin > taille) {
+		return 0;
+	}
+
+	for(int i = debut; i < fin-1; i++){
+		total += distance_entre_2_points(tab_latitudes[i], tab_longitudes[i], tab_latitudes[i+1], tab_longitudes[i+1], tab_altitudes[i], tab_altitudes[i+1]);
+
+}
+
+	return total;
+}
+
+int minimum_tableau(double tableau[], int taille, double* minimum){
+
+	if (taille <= 0) {
+		*minimum = 0;
+		return 0;
+	}
+
+	*minimum = tableau[0];
+	for (int i = 1; i < (taille); i++) {
+		if(*minimum > tableau[i]) {
+			*minimum = tableau[i];
+		}
+	}
+	return 1;
+}
+
+int maximum_tableau(double tableau[], int taille, double* maximum){
+
+	if (taille <= 0) {
+		*maximum = 0;
+		return 0;
+	}
+
+	*maximum = tableau[0];
+	for (int i = 1; i < (taille); i++) {
+		if(*maximum < tableau[i]) {
+			*maximum = tableau[i];
+		}
+	}
+	return 1;
+}
+
+double ascension(double tab_altitudes[], int taille) {
+	double tot = 0;
+	for(int i = 0; i < taille; i++) {
+		if(tab_altitudes[i+1] > tab_altitudes[i]) {
+			tot += (tab_altitudes[i+1] - tab_altitudes[i]);
+		}
+	}
+	return tot;
+}
