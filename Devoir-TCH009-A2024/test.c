1688eddacda7d9a3e84e76f7a8cbdeaa90aeda8e
LudovicConfaisCourcy <123512499+LudovicConfaisCourcy@users.noreply.github.com>
2024-11-30 13:05:25 -0500

    Update test.c

--- Devoir-TCH009-A2024/test.c
+++ Devoir-TCH009-A2024/test.c
@@ -6,3 +6,34 @@
 
 // Vous coderez ici vos fonctions de test
 
+void test_convertir_en_radian() {
+    
+}
+
+void test_distance_entre_2_points() {
+    
+}
+
+void test_afficher_afficher_tableau() {
+    
+}
+
+void test_distance_totale() {
+    
+}
+
+void test_ascension() {
+    
+}
+
+void test_pente_maximale() {
+    
+}
+
+void test_minimum_tableau() {
+    
+}
+
+void test_maximum_tableau() {
+    
+}
